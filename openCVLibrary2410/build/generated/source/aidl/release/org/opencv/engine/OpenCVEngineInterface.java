1eaff193b420eaa5c0888b74cd176141ee2e99c9
altugyigit <altug_yigit@hotmail.com>
2015-06-18 18:34:27 +0300

    icon değiştirildi.

--- openCVLibrary2410/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary2410/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/kerem/AndroidStudioProjects/BioMatch/openCVLibrary2410/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/altug/AndroidStudioProjects/BioMatch/openCVLibrary2410/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
